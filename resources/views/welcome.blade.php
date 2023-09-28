def50cc4876a3c300af644a9b97a6b827a3b631e
alaqsaka <aqsaabidin15@gmail.com>
2023-09-28 11:20:02 +0700

    visitor form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,30 @@
                 <p class="text-medium text-white font-semibold"> - Aqsa</p>
 
                 <p class="text-lg text-green-300 mt-10 font-bold">Guest Book</p>
+                <div>
+                    <p class="mt-1 text-sm leading-6 text-white">This information will be displayed publicly so be careful what you share.</p>
+                    <form>
+                        {{-- <input/> --}}
+                        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div class="sm:col-span-4">
+                              <label for="username" class="block text-sm font-medium leading-6 text-white">Nama</label>
+                              <div class="mt-2">
+                                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                  <input type="text" name="username" id="username" autocomplete="username" class="rounded-md bg-white block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-full">
+                            <label for="about" class="block text-sm font-medium leading-6 text-white">Note</label>
+                            <div class="mt-2">
+                                <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            </div>
+                            <p class="mt-3 text-sm leading-6 text-white">Write a few sentences for owner</p>
+                        </div>
+
+                        <button type="submit" class="rounded-md bg-green-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Save</button>
+                    </form>
+                </div>
             </div>
         </div>
     </body>
