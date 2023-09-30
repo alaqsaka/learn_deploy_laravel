b320481605ad9f043cbedb6de7aa71e3663565a1
alaqsaka <aqsaabidin15@gmail.com>
2023-09-30 22:27:00 +0700

    form create menu

--- resources/views/menus/create.blade.php
+++ resources/views/menus/create.blade.php
@@ -12,24 +12,32 @@
 
                <form method="POST" action="/tenant" enctype="multipart/form-data">
                 @csrf
-               <div class="mb-3 flex flex-col gap-5">
-                <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Nama Pemilik Tenant</label>
-                <input name="owner" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-lg ring-offset-background file:border-0 file:bg-transparent file:text-lg file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"/>
-                @error('owner')
-                <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
-            @enderror
-               </div>
+                <div class="mb-3 flex flex-col gap-5">
+                    <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Tenant ID</label>
+                    <input disabled name="tenant_id" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-lg ring-offset-background file:border-0 file:bg-transparent file:text-lg file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"/>
+                    @error('tenant_id')
+                    <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
 
                <div class="mb-3 flex flex-col gap-5">
-                <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Nama Tenant</label>
+                <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Nama Menu</label>
                 <input name="name" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-lg ring-offset-background file:border-0 file:bg-transparent file:text-lg file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"/>
                 @error('name')
                 <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
+                @enderror
+               </div>
+
+               <div class="mb-3 flex flex-col gap-5">
+                <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Harga Menu</label>
+                <input name="price" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-lg ring-offset-background file:border-0 file:bg-transparent file:text-lg file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"/>
+                @error('price')
+                <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
             @enderror
                </div>
 
                <div class="mb-3 flex flex-col gap-5">
-                <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Deskripsi</label>
+                <label class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Deskripsi Menu</label>
                 <textarea name="description" class="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
 
                 </textarea>
@@ -40,7 +48,7 @@
 
                <div class="mb-6">
                 <label for="imageUrl" class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Tenant Foto Profile
+                    Foto Menu
                 </label>
                 <input
                     type="file"
